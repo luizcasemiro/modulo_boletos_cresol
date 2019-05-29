ae22287eb368bb7f099f2c55a3562ff824ab542d
a <a>
2019-05-29 01:50:59 -0300

    Initial

---
+++ boletos/index.js
@@ -0,0 +1,10 @@
+class BoletosCresol{
+
+    constructor(credentials){
+        const { login, password } = credentials
+        this.login = login
+        this.password = password
+    }
+
+}
+module.exports = BoletosCresol
